e6658481632d1ac5b164ecc2b6e7b0c4667f8985
cduhassanbaydoun <hassanbaydoun_936@hotmail.com>
2015-04-12 00:11:10 +0930

    Update pgRegister.php

--- pages/pgRegister.php
+++ pages/pgRegister.php
@@ -8,19 +8,26 @@
 		<input type="text" name="username" id="username" />
 
 		<label for="username">SurName</label>
-		<input type="text" name="username" id="username" />
+		<input type="text" name="surname" id="surname" />
          
                 <label for="username">Age</label>
-		<input type="text" name="username" id="username" />
+		<input type="text" name="age" id="age" />
 
 		<label for="username">Work number</label>
-		<input type="text" name="username" id="username" />
+		<input type="number" name="worknumber" id="worknumber" />
 		
                 <label for="username">Mobile Number</label>
-		<input type="text" name="username" id="username" />
+		<input type="number" name="mobilenumber" id="mobilenumber" />
+		
+		<label for="username">Job Status</label>
+		<input type="text" name="jobstatus" id="jobstatus" />
 
                 <label for="password">Password</label>
 		<input type="password" name="password" id="password" />
+		
+		<label for="password">Re Type Password</label> 
+		<input type="password" name="password" id="password" /> 
+
 	
 	</form>
 </div>
