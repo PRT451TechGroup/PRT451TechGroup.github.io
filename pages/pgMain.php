96b9b8d3a98c83e5ae81292b36df3040f0556cc4
cduhassanbaydoun <hassanbaydoun_936@hotmail.com>
2015-04-12 00:26:59 +0930

    Update pgMain.php

--- pages/pgMain.php
+++ pages/pgMain.php
@@ -51,8 +51,45 @@
 		</div>
 	</div>
 	<div id="tabCreate" class="ui-content">
-		<form>
-			<label for=""></label>
-		</form>
+		<div data-role="tabs">
+			<div data-role="navbar">
+				<ul>
+					<li><a href="#tabOverviewActive">Active</a></li>
+					<li><a href="#tabOverviewCompleted">Completed</a></li>
+				</ul>
+			</div>
+			<div id="tabOverviewActive" class="ui-content">
+				<table data-role="table" id="tblActive" data-filter="true">
+					<thead>
+						<tr>
+							<th>Asset</th>
+							<th data-priority="1">Description</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1234888</td>
+							<td>abcdefgh</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div id="tabOverviewCompleted" class="ui-content">
+				<table data-role="table" id="tblCompleted" data-filter="true">
+					<thead>
+						<tr>
+							<th>Asset</th>
+							<th data-priority="1">Description</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>123777</td>
+							<td>abcdefgh</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
 	</div>
 </div>
