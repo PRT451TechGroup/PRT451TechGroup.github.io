652e826aa88d5c92a1c4b0de3716f0783e38ee5c
cduhassanbaydoun <hassanbaydoun_936@hotmail.com>
2015-04-11 11:54:37 +0930

    Update pgRegister.php

--- pages/pgRegister.php
+++ pages/pgRegister.php
@@ -3,5 +3,21 @@
 </div>
 <div data-role="content">
 	<form>
+
+		<label for="username">Name</label>
+		<input type="text" name="username" id="username" />
+
+		<label for="username">SurName</label>
+		<input type="text" name="username" id="username" />
+         
+                <label for="username">Age</label>
+		<input type="text" name="username" id="username" />
+
+                <label for="username">Mobile Number</label>
+		<input type="text" name="username" id="username" />
+
+                <label for="password">Password</label>
+		<input type="password" name="password" id="password" />
+	
 	</form>
 </div>
