193a16123a8c049862cc43f13d172475e89e6c9a
AkariAkaori <kaori8814@gmail.co>
2015-04-20 23:16:22 +0930

    api

--- api.php
+++ api.php
@@ -1,5 +1,47 @@
 <?php
 
-function 
+require_once 'config.inc.php';
+
+class DataManager
+{
+	private $host;
+	private $database;
+	private $user;
+	private $password;
+	
+	public function __construct($h, $d, $u, $p)
+	{
+		$this->host = $h;
+		$this->database = $d;
+		$this->user = $u;
+		$this->password = $p;
+	}
+	private function open_connection()
+	{
+		return new mysqli($this->host, $this->user, $this->password, $this->database);
+	}
+	public function new_job($location, $building, $floor, $room, $duedate, $noequipment, $assetno, $specification)
+	{
+		$conn = open_connection();
+		
+		if ($conn->connect_error)
+		{
+			die("Connect Error: {$conn->connect_error}");
+		}
+		
+		$stmt = $conn->stmt_init();
+		
+		if ($stmt->prepare('INSERT INTO Jobs (Location, Building, Floor, Room, DueDate, NoEquipment, AssetNo, Specification) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'))
+		{
+			$stmt->bind_param('siiisiss', $location, $building, $floor, $room, $duedate, $noequipment, $assetno, $specification);
+			$stmt->execute();
+			$stmt->close();
+		}
+		
+		$conn->close();
+	}
+}
+
+$dbm = new DataManager($cfg['mysql']['host'], $cfg['mysql']['database'], $cfg['mysql']['user'], $cfg['mysql']['password']);
 
 ?>
---
+++ config.inc.php
@@ -0,0 +1,6 @@
+<?php
+$cfg['mysql']['host'] = 'localhost';
+$cfg['mysql']['database'] = 'prt451gr_db';
+$cfg['mysql']['user'] = 'prt451gr_db';
+$cfg['mysql']['password'] = 'set on spinetail do not push the password to github';
+?>
